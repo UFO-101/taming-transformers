a2071efb83ad57b46d0073d2a0595de8dbcc42a6
UFO-101 <47218308+UFO-101@users.noreply.github.com>
2021-07-30 00:15:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ufo101_taming_transformers',
+    name='ufo101TamingTransformers',
     version='0.0.1',
     description='Taming Transformers for High-Resolution Image Synthesis',
     packages=find_packages(),
